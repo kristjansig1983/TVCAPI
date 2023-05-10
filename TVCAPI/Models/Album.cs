7132954930f200dcf9b279c3fd597b719adc3e85
kristjansig1983 <kristjansig1983@gmail.com>
2023-05-10 08:26:35 +0000

    Little dont. Added Songs.cs and put more code in Album.cs

--- TVCAPI/Models/Album.cs
+++ TVCAPI/Models/Album.cs
@@ -8,5 +8,7 @@ namespace TVCAPI.Models
         [MaxLength(255)]
         public string AlbumName { get; set; }
         public int AlbumYear { get; set; }
+        public List<Songs> Songs { get; set; } = new();
+        public string AlbumCoverUrl { get; set; }
     }
 }
---
+++ TVCAPI/Models/Songs.cs
@@ -0,0 +1,6 @@
+﻿namespace TVCAPI.Models
+{
+    public class Songs
+    {
+    }
+}
